429270d3fa7e7ea7dd3253940a6140d8ca9df0c7
roy motsi <kuroy005@hotmail.com>
2020-05-03 00:02:24 -0400

    New Person class

---
+++ Section 2/Person.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section_2
+{
+    class Person
+    {
+        public string FirstName;
+        public string LastName;
+        public int Age;
+        public Person Spouse;
+
+        public static int Count;
+        public static int SumOfAllAges;
+
+        public static double AverageAge()
+        {
+            return (double)Person.SumOfAllAges / (double)Person.Count;
+        }
+
+        public string GetFullName()
+        {
+            return this.FirstName + " " + this.LastName;
+        }
+
+        public void PrintNameAndAge()
+        {
+            Console.WriteLine(this.GetFullName() + " " + this.Age); 
+        }
+        public  void AskUserForSpouseNameAndAge() 
+        {
+            Console.Write("Enter Your Spouses Name          : ");
+            this.FirstName = System.Console.ReadLine();
+            Console.Write("Enter Your spouses Age           : ");
+            this.Age = int.Parse(System.Console.ReadLine());
+        }
+
+        public void WhatIsYourNameAndAge()
+        {
+           Console.Write("Enter Your First Name             : ");
+            this.FirstName = System.Console.ReadLine();
+
+           Console.Write("Enter Your Last Name              : ");
+            this.LastName = System.Console.ReadLine();
+
+           Console.Write("Enter Your Age                    : ");
+            this.Age = int.Parse(System.Console.ReadLine());
+            
+            Person.Count++;
+            Person.SumOfAllAges += this.Age;
+        }
+
+        public void CreateSpouseIfMarried()
+        {
+            Console.Write("Are you married? (y/n) : ");
+
+            if (Console.ReadLine().ToLower().StartsWith("y"))
+            {
+                this.Spouse = new Person();
+                this.Spouse.AskUserForSpouseNameAndAge();
+                this.Spouse.Spouse = this;
+                this.Spouse.LastName = this.LastName;
+            }
+
+
+
+        }
+    }
+}
--- Section 2/Program.cs
+++ Section 2/Program.cs
@@ -6,7 +6,40 @@ namespace Section_2
     class Program
     {
         static void Main(string[] args)
-        {
+        {            
+            Console.WriteLine("[Enter Information for p1]");
+            Console.WriteLine("");
+
+            Person person1 = new Person();
+            person1.WhatIsYourNameAndAge(); 
+            person1.CreateSpouseIfMarried();
+            
+            Console.WriteLine("");
+            Console.WriteLine("[Enter Information for p2]");
+            Console.WriteLine("");
+
+            Person person2 = new Person();
+            person2.WhatIsYourNameAndAge();
+            person2.CreateSpouseIfMarried();
+
+            Console.WriteLine("");
+            Console.WriteLine("[Printing Results]");
+            Console.WriteLine("");
+
+            person1.PrintNameAndAge();
+            person1.Spouse.PrintNameAndAge();
+            person2.PrintNameAndAge();
+            person2.Spouse.PrintNameAndAge();
+
+            Console.WriteLine("Average Age: " + Person.AverageAge());
+            Console.ReadKey();
+
+
+
+
+
+
+
         }
     }
 }
