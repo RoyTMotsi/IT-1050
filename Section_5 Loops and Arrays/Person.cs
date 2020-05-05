73921e467aae17e3d16c5214f99dabc71bcee620
roy motsi <kuroy005@hotmail.com>
2020-05-05 00:55:53 -0400

    Section 5 Commit

---
+++ Section_5 Loops and Arrays/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace Section_5_Loops_and_Arrays
+{
+    class Person
+    {
+        public string firstName = "name";
+        public string lastName = "last";
+
+        public Person(string FirstName, string LastName)
+        {
+            firstName = FirstName;
+            lastName = LastName;
+        }
+
+        public void Print()
+        {
+            string fullName = firstName + " " + lastName;
+            System.Console.WriteLine(fullName);
+        }
+    }
+}
--- Section_5 Loops and Arrays/Program.cs
+++ Section_5 Loops and Arrays/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Section_5_Loops_and_Arrays
 {
@@ -10,6 +7,19 @@ namespace Section_5_Loops_and_Arrays
     {
         static void Main(string[] args)
         {
+            Person[] someperson = new Person[3];
+            someperson[0] = new Person("Mike", "Tyson");
+            someperson[1] = new Person("Floyd", "Mayweather");
+            someperson[2] = new Person("Lebron", "James");
+
+
+            foreach (Person person in someperson)
+            {
+                person.Print();
+            }
+
+            System.Console.ReadLine();
+
         }
     }
 }
