2bf266d067522f2da8fc8284043b06b2a6bce575
roy motsi <kuroy005@hotmail.com>
2020-05-04 23:48:40 -0400

    New commit

---
+++ Section_5 Loops and Arrays/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section_5_Loops_and_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
